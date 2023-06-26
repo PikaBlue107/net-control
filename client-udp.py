d32ba42a3786e168515c7d3b5e60e2f777d20096
Melody Griesen <pikablue107@gmail.com>
2023-06-26 09:05:02 -0700

    config: setup client for talking across network

--- client-udp.py
+++ client-udp.py
@@ -2,7 +2,7 @@ import socket
 
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 20001)
+serverAddressPort   = ("192.168.137.1", 20001)
 bufferSize          = 1024
 
 # Create a UDP socket at client side
